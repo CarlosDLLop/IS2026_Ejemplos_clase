5aba353f9a96aae65dc92977e3a2beb2baa4de9b
CarlosDLLop <carlos83447@gmail.com>
2025-11-11 18:38:13 -0600

    ft:Cambios en el index principal

--- js/js_producto.js
+++ js/js_producto.js
@@ -1,8 +1,8 @@
 const productos =[{nombre:"peluche", precio:20, img:"https://minisomx.vtexassets.com/arquivos/ids/385937-800-800?v=638966254469430000&width=800&height=800&aspect=true"}, 
 {nombre: "playera", precio:200, img:"https://atoledementa.com/wp-content/uploads/2022/06/5D566907-5281-4D36-A7FA-4E8E2A7E6968-scaled.jpeg"}, 
+{nombre:"peluche", precio:20, img:"https://minisomx.vtexassets.com/arquivos/ids/385937-800-800?v=638966254469430000&width=800&height=800&aspect=true"}, 
 {nombre: "playera", precio:200, img:"https://atoledementa.com/wp-content/uploads/2022/06/5D566907-5281-4D36-A7FA-4E8E2A7E6968-scaled.jpeg"}, 
-{nombre: "playera", precio:200, img:"https://atoledementa.com/wp-content/uploads/2022/06/5D566907-5281-4D36-A7FA-4E8E2A7E6968-scaled.jpeg"}, 
-{nombre: "playera", precio:200, img:"https://atoledementa.com/wp-content/uploads/2022/06/5D566907-5281-4D36-A7FA-4E8E2A7E6968-scaled.jpeg"}
+{nombre:"peluche", precio:20, img:"https://minisomx.vtexassets.com/arquivos/ids/385937-800-800?v=638966254469430000&width=800&height=800&aspect=true"}
 ]; 
 
 const contenedor =document.getElementById("contenedor"); 
